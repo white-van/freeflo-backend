9b97448b2591f25a6b5e7fa5ef6243fa45264fa7
Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>
2021-01-16 08:35:18 -0500

    Create json views (#2)
    * add missing names for schema
    
    * add json view for projects
    
    * add pull request show json
    
    * add remaining jsons
    
    * fix changes
    
    * fixed-2
    
    * add suggested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  api_guard_routes for: 'users'
+  api_guard_routes for: 'users', controller: {
+  registration: 'users/registration'
+  }
 
-  resources :user do
+  resources :users do
     get '/projects', to: 'projects#index'
     get '/pull_requests', to: 'pull_requests#index'
     get '/commits', to: 'versions#index'
