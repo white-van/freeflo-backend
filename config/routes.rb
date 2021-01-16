b6f7ec8abddb133b3852800cabb86c181a04050c
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-01-16 17:09:18 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   api_guard_routes for: 'users', controller: {
-  registration: 'users/registration'
+    registration: 'users/registration'
+    authentication: 'users/authentication'
   }
 
   resources :users do
