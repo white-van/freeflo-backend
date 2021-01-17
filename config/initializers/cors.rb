03a42b08c257536580c82d56e5daf1f5d8428d3a
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-01-16 20:43:21 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,12 +4,9 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-Rails.application.config.hosts << "https://freeflo-backend.herokuapp.com"
-
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
      origins '*'
-
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
