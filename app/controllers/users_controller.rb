7be5aef6b54f41f3d7fe761f79445c1cb566d57d
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-01-16 22:25:03 -0500

    fix user controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render 'projects/show', status: :created
+    render 'users/show', status: :created
   end
 
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render 'projects/show', status: :created
+      render 'users/show', status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
