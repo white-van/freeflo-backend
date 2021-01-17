3d1eb3d74eaea507296c0cb62d1644ff1b5ccbe7
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-01-16 20:19:48 -0500

    re add test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+Rails.application.config.hosts << "http://localhost:3000"
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
