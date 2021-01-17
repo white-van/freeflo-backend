2dcfd72a50918025c7c93bb9ad47e4ee50a3d244
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-01-16 20:24:41 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.hosts << "http://localhost:3000"
+Rails.application.config.hosts << "freeflo-backend.herokuapp.com"
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
